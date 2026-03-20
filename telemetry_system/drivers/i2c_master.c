786f4a2f8b9fcf6b7fe9a2e53c89bf38a3aab60e
vamsi <vamsikiran.mekala2@mail.dcu.ie>
2026-03-20 18:45:56 +0000

    I2C master has been enabled

---
+++ telemetry_system/drivers/i2c_master.c
@@ -0,0 +1,52 @@
+#include <fcntl.h>      // For open()
+#include <unistd.h>     // For read(), write(), close()
+#include <sys/ioctl.h>  // For ioctl()
+#include <linux/i2c-dev.h> // For I2C_SLAVE constant
+#include <i2c/smbus.h>
+#include <stdio.h>      // For printf/debugging
+#include <stdint.h>
+
+int fd;
+//Opening handle for i2c device
+int init() {
+    fd = open("/dev/i2c-1",O_RDWR);
+    if(fd < 0) {
+        perror("error");
+        return -1;
+    }
+    return fd;
+}
+
+//write fn has fd device address, register address and data to send
+int I2Cwrite(int fd,uint8_t device_addr,uint8_t reg_addr,uint8_t data) {
+    uint8_t buffer[2];
+    buffer[0] = reg_addr;
+    buffer[1] = data;
+    if(ioctl(fd,I2C_SLAVE,device_addr) < 0) {
+        perror("ioctl");
+        return -1;
+    }
+    if(write(fd,buffer,2) != 2) {
+        perror("error");
+        return -1;
+    }
+    return 0;
+}
+
+
+int I2Cread(int fd, uint8_t device_addr, uint8_t *reg_addr, uint8_t *data) {
+    
+    if(ioctl(fd,I2C_SLAVE,device_addr) < 0) {
+        perror("ioctl");
+        return -1;
+    }
+    if(write(fd,reg_addr,1) != 1) {
+        perror("error");
+        return -1;
+    }
+     if(read(fd,data,1) != 1) {
+        perror("error");
+        return -1;
+    }
+    return 0;
+}
